9d5216893d420413356d4e5783ea01fb563e3dc8
Marya <maryammuchai@gmail.com>
2021-10-21 14:28:30 +0300

    calling the pokeapi to return array of results

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 // import logo from './logo.svg';\
+import React, {useState,useEffect} from 'react';
 
 function App() {
 
+const [allPokemons, setAllPokemons] = useState([])
+const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
 
+const getAllPokemons = async () => {
+  const res = await fetch(loadMore)
+  const data = await res.json()
 
+  console.log(data)
+}
+useEffect(() => {
+ getAllPokemons()
+}, [])
   return (
     <div className="app-container">
      <h1>Pokemon App</h1>
