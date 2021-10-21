981f6896cec63f7e3d67144a3b1802803c8d1d84
Marya <maryammuchai@gmail.com>
2021-10-21 14:42:46 +0300

    component to display pokemon

--- src/App.js
+++ src/App.js
@@ -10,8 +10,19 @@ const getAllPokemons = async () => {
   const res = await fetch(loadMore)
   const data = await res.json()
 
-  console.log(data)
-}
+  setLoadMore(data.next)
+  function createPokemonObject (result) {
+    result.forEach(async pokemon => {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+      const data = await res.json()
+
+      setAllPokemons( currentList => [...currentList, data])
+      await allPokemons.sort((a, b) => a.id - b.id)
+    })
+  }
+  createPokemonObject(data.results)
+  }
+
 useEffect(() => {
  getAllPokemons()
 }, [])
---
+++ src/components/Thumb.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PokemonThumb = ({ image, name, type, _callback }) => {
+    const style = type + " thumb-container";
+    return (
+        <div className={style}>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h3>{name}</h3>
+                <small>Type: {type}</small>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonThumb
